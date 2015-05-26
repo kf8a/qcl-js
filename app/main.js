b93c01acf4c245f309d274ee17def4675913db10
Sven Bohm <bohms@msu.edu>
2015-05-26 15:10:33 -0400

    copied more boilderplate from ohaus.js

---
+++ app/main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+import React from "react";
+import SocketService from "./socket-service.js"
+// import App from "./ohaus.jsx"
+
+
+import './main.css';
+
+main();
+
+function main() {
+  var socketService = new SocketService({
+    path: '/ws',
+    port: "8081"
+  });
+
+  React.render(
+    < App socketService={socketService} />, 
+      document.getElementById('app')
+  )
+}
